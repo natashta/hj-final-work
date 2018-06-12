a9366f5b12364b45598506bb4328a419221f70d1
Natalia <natashta@gmail.com>
2018-06-12 14:09:40 +0300

    Последняя версия на 12.06.
    Нет функции комментирования. В режиме рисования не выбирается цвет. 
    Главный вопрос - придумать, как сделать маску для рисунков и комментариев. (Создать новый див?)

--- js/solution.js
+++ js/solution.js
@@ -1,29 +1,33 @@
 'use strtict'
 
-const commOn = document.querySelector('#comments-on'),
+const formComments = document.querySelectorAll('.comments__form');
+	divComments = document.querySelector('.comments__body'),
+	commOn = document.querySelector('#comments-on'),
 	commOff = document.querySelector('#comments-off'),
 	wrap = document.querySelector('.wrap'),
 	menu = document.querySelector('ul.menu'),
 	burger = document.querySelector('.burger'),
-	//drag = document.querySelector('.drag'),
 	currentImg = document.querySelector('.current-image'),
 	error = document.querySelector('.error'),
 	loader = document.querySelector('.image-loader'),
 	canvas = document.querySelector('#canvas'),
 	ctx = canvas.getContext('2d'),
-	divComments = document.querySelector('.comments__body'),
 	imgInput = document.querySelector('#imgInput'),
 	serverUrl = 'https://neto-api.herokuapp.com',
 	socketUrl = 'wss://neto-api.herokuapp.com/pic/';
 
+	let currColor;
 
-//Состояние Публикация
+
+//Режим публикации
 currentImg.src = '';
-wrap.removeChild(document.querySelector('.comments__form')); 
+removeComments();
+//wrap.removeChild(formComments); 
 //hideEl(burger);
 hideEl(document.querySelector('.comments'));
 hideEl(document.querySelector('.draw'));
 hideEl(document.querySelector('.share'));
+
 
 function hideEl(el) {
 	el.style.display = 'none';
@@ -33,8 +37,8 @@ function showEl(el) {
 	el.style.display = '';
 }
 
-//меню
 burger.addEventListener('click', burgerWrap)
+
 
 function burgerWrap() {
 	menu.dataset.state = 'default';
@@ -44,14 +48,18 @@ function burgerWrap() {
 		modeItem.dataset.state = '';
 		showEl(modeItem);
 	})
+	
 	Array.from(document.querySelectorAll('.tool')).forEach(toolItem => toolItem.dataset.state = '');
 }
 
-//переключение меню
+//Переключатели
+//Меню в режиме рецензирования
 showMenu()
 function showMenu() {
+		
 	Array.from(document.querySelectorAll('.mode')).forEach(modeItem => {
 		modeItem.dataset.state = '';
+		document.querySelector('.menu__item.tool').dataset.state = '';
 		modeItem.addEventListener('click', () => {
 				menu.dataset.state = 'selected';
 				modeItem.dataset.state = 'selected';
@@ -60,37 +68,36 @@ function showMenu() {
 		})
 	})
 }
-
 /*
 document.querySelector('.comments').addEventListener('click', ()=> {
 	hideEl(document.querySelector('.new'));
 	hideEl(document.querySelector('.draw'));
 	hideEl(document.querySelector('.share'));
-	showTools();
+	document.querySelector('.menu__item.tool.comments-tools').dataset.state = 'selected';
+	document.querySelector('.menu').dataset.state = 'selected';
+	document.querySelector('.menu__item').dataset.state = 'selected';
 });
 
 document.querySelector('.draw').addEventListener('click', ()=> {
 	hideEl(document.querySelector('.new'));
 	hideEl(document.querySelector('.comments'));
 	hideEl(document.querySelector('.share'));
-	showTools();
+	document.querySelector('.menu__item.tool.draw-tools').dataset.state = 'selected';
+	document.querySelector('.menu').dataset.state = 'default';
+	document.querySelector('.menu__item').dataset.state = 'selected';
+	document.querySelector('.menu[data-state="default"] > .mode').dataset.state = 'selected';
 });
 
 document.querySelector('.share').addEventListener('click', ()=> {
 	hideEl(document.querySelector('.new'));
 	hideEl(document.querySelector('.draw'));
 	hideEl(document.querySelector('.comments'));
-	showTools();
-});
-
-function showTools() {
-	showEl(burger);
+	document.querySelector('.menu__item.tool.share-tools').dataset.state = 'selected';
 	document.querySelector('.menu').dataset.state = 'selected';
 	document.querySelector('.menu__item').dataset.state = 'selected';
-	document.querySelector('.menu__item.tool').dataset.state = 'selected';
-	showEl(document.querySelector('.tool'));
-}
+});
 */
+
 
 // Перемещение меню
 let movedPiece = null;
@@ -114,7 +121,7 @@ function dragStart(event) {
 	maxY = wrap.offsetTop + wrap.offsetHeight - movedPiece.offsetHeight;
 	shiftX = event.pageX - event.target.getBoundingClientRect().left - window.pageXOffset;
 	shiftY = event.pageY - event.target.getBoundingClientRect().top - window.pageYOffset;
-	}
+}
 }
 
 function dragMenu(event) {
@@ -128,7 +135,7 @@ function dragMenu(event) {
 	y = Math.max(y, minY);
 	movedPiece.style.left = x + 'px';
 	movedPiece.style.top = y + 'px';
-	}
+}
 }
 
 function dropMenu(event) {
@@ -145,6 +152,7 @@ wrap.addEventListener('dragover', event => event.preventDefault());
 function loadImg(event) {
 	hideEl(error);
 	const files = Array.from(event.target.files);
+
 	sendFile(files);
 	}
 
@@ -152,8 +160,8 @@ function dropImg(event) {
 	hideEl(error);
 	event.preventDefault();
 	const files = Array.from(event.dataTransfer.files);
+
 	if (currentImg.dataset.state === 'load') {
-		console.log('nope!')
 		showEl(error);
 		error.lastElementChild.textContent = 'Чтобы загрузить новое изображение, пожалуйста, воспользуйтесь пунктом "Загрузить новое" в меню';
 		return;
@@ -166,22 +174,24 @@ const formData = new FormData();
 	
 files.forEach(file => {
 	if ((file.type === 'image/jpeg') || (file.type === 'image/png')) {
-		formData.append('image', file);
-		formData.append('load', 'load');
-		//canvas.style.backgroundImage=`url(${URL.createObjectURL(file)})`;
-		currentImg.src = URL.createObjectURL(file);
-		currentImg.addEventListener('load', event => {
-		URL.revokeObjectURL(event.target.src);
-	} else {
-		showEl(error);	
-	}
+	formData.append('image', file);
+	formData.append('load', 'load');
+	currentImg.src = URL.createObjectURL(file);
+	currentImg.addEventListener('load', event => {
+	URL.revokeObjectURL(event.target.src);
+	currentImg.dataset.state = 'load'
+	
+});
+} else {
+	showEl(error);
+}
 });
 
 const xhr = new XMLHttpRequest();
 xhr.open('POST', serverUrl);
 xhr.addEventListener('load', () => {
 	if (xhr.status === 200) {
-		console.log(`Файл сохранен`);
+		console.log('load')
 	}
 	});
 xhr.addEventListener("error", () => {
@@ -192,22 +202,66 @@ xhr.addEventListener('loadstart', () => {
 });
 xhr.addEventListener('loadend', () => {
 	hideEl(loader);
+	onLoadImg();
 });
 xhr.send(formData);
 }
 
-function setcurrentImage(fileInfo) {
-	currentImg.src = fileInfo.url;
+function onLoadImg() {
+if (currentImg.dataset.state === 'load') {
+	hideEl(document.querySelector('.new'));
+	hideEl(document.querySelector('.draw'));
+	hideEl(document.querySelector('.comments'));
+	document.querySelector('.menu__item.tool.share-tools').dataset.state = 'selected';
+	document.querySelector('.menu').dataset.state = 'selected';
+	document.querySelector('.menu__item').dataset.state = 'selected';
+}
 }
 
+//Логика: после загрузки картинки клик в любом месте холста(картинки) 
+//открывает функцию комментирования либо функцию рисования в зависимости от открытого пункта меню
+//эта штука не дает кликнуть на меню
+/*wrap.addEventListener('click', (event) => {
+	event.preventDefault();
+})*/
+
+//Убираем комментарии
+function removeComments() {
+	//const formComments = wrap.querySelectorAll('.comments__form');
+	Array.from(formComments).forEach(form => {form.remove()});
+}
+
+//скрыть-открыть комментарии
+commOn.addEventListener('click', commentsOn);
+commOff.addEventListener('click', commentsOff);
+
+function commentsOff() {
+	//const forms = document.querySelectorAll('.comments__form');
+	Array.from(formComments).forEach(form => {
+		form.style.display = 'none';
+	 })
+}
+
+function commentsOn() {
+	//const forms = document.querySelectorAll('.comments__form');
+	Array.from(formComments).forEach(form => {
+		form.style.display = '';
+	})
+}
+
+//Клик на экране - комменты. Надо создать форму для комментов и при клике ее аппенд.
+
+
 //выбор цвета для рисования
-let currColor;
 Array.from(menu.querySelectorAll('.menu__color')).forEach(color => {
-	 	color.addEventListener('click', () => {
-		currColor = document.querySelector('.draw-tools input[type="radio"]:checked').value; 
-		console.log(currColor);
+	 	color.addEventListener('change', () => {
+	 		if (color.checked) {
+		currColor = document.querySelector('.draw-tools input[type="radio"]:checked').value;
 		return currColor;
-	});	
+		}
+	});
+	console.log(currColor); 
+	return currColor;	
 });
 
 //рисование
@@ -241,13 +295,16 @@ canvas.addEventListener('mouseleave', () => {
   drawing = false;
 })
 
+
 canvas.addEventListener('mousedown', e => {
+	e.preventDefault();
   curves.push([e.offsetX, e.offsetY]);
   drawing = true;
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.arc(e.offsetX, e.offsetY, brush_radius/2, 0, 2 * Math.PI);
   ctx.fill();
+  console.log(color);
 });
 
 canvas.addEventListener('mousemove', e => { 
